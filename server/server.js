fc7655164758ca5a7f579843c053a0aff8dfba1b
conradj <conradsjackson@gmail.com>
2015-02-20 13:09:57 +0000

    initial commit

---
+++ server/server.js
@@ -0,0 +1,36 @@
+var loopback = require('loopback');
+var boot = require('loopback-boot');
+//var sse = require('server-sent-events');
+var app = module.exports = loopback();
+var socketIO = require('socket.io');
+
+// Bootstrap the application, configure models, datasources and middleware.
+// Sub-apps like REST API are mounted via boot scripts.
+boot(app, __dirname);
+
+app.start = function() {
+  // start the web server
+  return app.listen(function() {
+    app.emit('started');
+    console.log('Web server listening at: %s', app.get('url'));
+  });
+};
+
+// start the server if `$ node server.js`
+if (require.main === module) {
+  var server = app.start();
+   // initialize socket.io and store it in the app instance
+  app.io = socketIO(server);
+    
+    app.io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+}
+
+/*app.use('/room', function(req, res, next ) { 
+    console.log("doestnsi work");
+  res.sse("dsds"); 
+    console.log("doestnsi work2");
+    return;
+})*/
