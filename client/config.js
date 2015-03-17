edaf107149919aec0493c75c751e0af49a6cfb31
conradj <conradsjackson@gmail.com>
2015-03-17 06:28:54 +0000

    tidy up dist files

--- client/config.js
+++ client/config.js
@@ -3,16 +3,6 @@ System.config({
   "paths": {
     "*": "*.js",
     "github:*": "jspm_packages/github/*.js"
-  },
-  "bundles": {
-    "build": [
-      "lib/display-room",
-      "lib/ajax",
-      "lib/sockets",
-      "lib/display-rooms",
-      "lib/bootstrap",
-      "lib/main"
-    ]
   }
 });
 
