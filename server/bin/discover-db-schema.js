479d2beb469bc47c3644c9c63f79aac4dc0f7301
conradj <conradsjackson@gmail.com>
2015-05-17 12:23:07 +0100

    Discovered models

---
+++ server/bin/discover-db-schema.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var fs = require('fs');
+var server = require(path.resolve(__dirname, '../server'));
+var loopback = require('loopback');
+var ds = server.dataSources.db;
+var app = module.exports = loopback();
+
+ds.discoverModelDefinitions(function (err, models) {
+  models.forEach(function (def) {
+      // def.name ~ the model name
+      ds.discoverSchema(def.name, null, function (err, schema) {
+        if ( ['acl', 'accesstoken', 'role', 'rolemapping', 'test', 'user'].indexOf( def.name ) == -1 ) {
+          // make sure the model property names retain the same casing as the DB columns
+          for (var prop in schema.properties) {
+            if(schema.properties[prop].postgresql) {
+              var copyprop = schema.properties[prop];
+              schema.properties[schema.properties[prop].postgresql.columnName] = copyprop;
+              delete schema.properties[prop];
+            }
+          }
+          if(schema.properties.ID) {
+            schema.properties.ID.required = false;
+          }
+          if(schema.name) { // make the model name have the same casing as table names
+            schema.name = def.name;
+          }
+          //console.log(JSON.stringify(schema, null, 2));
+          fs.writeFileSync('common/models/' + def.name + '.json', JSON.stringify(schema, null, 2), 'utf-8');
+          console.log("model created:", def.name);
+        }
+      });
+  });
+});
