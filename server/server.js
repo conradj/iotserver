849a92a58904ac19818ff9932e61b642d014e0e4
conradj <conradsjackson@gmail.com>
2015-10-07 22:12:12 +0100

    get data

--- server/server.js
+++ server/server.js
@@ -29,11 +29,11 @@ var server = http.createServer(function (req, res) {
   
        pg.connect(conString, function(err, client) {
          console.log('connect');
-         var query = client.query('SELECT NOW() AS "theTime"', function(err, result) {
+         var query = client.query('SELECT "Name" FROM "public"."Genre" LIMIT 100', function(err, result) {
             if (err) throw err;
             console.log('callback');
             res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(result.rows[0].theTime + "\n");
+            res.write(result.rows[0].Name + "\n");
             res.write(process.env.NODE_ENV + "\n");
             res.end('Hello Worldww\n');
             });
