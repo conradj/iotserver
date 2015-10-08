68d8e2b1a22a06c2003dff4f53fb07767e69ac22
conradj <conradsjackson@gmail.com>
2015-10-08 16:04:44 +0100

    azure

--- server/server2.js
+++ server/server2.js
@@ -2,39 +2,24 @@ var http = require('http')
 var port = process.env.PORT || 1337;
 var pg = require('pg');
 var conString = process.env.ELEPHANTSQL_URL || "";
-// var client = new pg.Client(conString);
-// 
-// http.createServer(function(req, res) {
-//   client.connect(function(err) {
-//     if(err) {
-//       return console.error('could not connect to postgres', err);
-//     }
-//     client.query('SELECT NOW() AS "theTime"', function(err, result) {
-//       if(err) {
-//         return console.error('error running query', err);
-//       }
-//       res.write(process.env.NODE_ENV + "\n");
-//       console.log(result.rows[0].theTime);
-//       res.write(result.rows[0].theTime + "\n");
-//       res.write('Hello World\n');
-//       //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
-//       //client.end();
-//     });
-//   });
-// }).listen(port);
-
-var pg = require('pg');
+var loopback = require('loopback');
+var boot = require('loopback-boot');
+//var sse = require('server-sent-events');
+var app = module.exports = loopback();
+var socketIO = require('socket.io');
 
 var server = http.createServer(function (req, res) {
   
        pg.connect(conString, function(err, client) {
          console.log('connect');
+         console.log(port);
          var query = client.query('SELECT "Name" FROM "public"."Genre" LIMIT 100', function(err, result) {
             if (err) throw err;
             console.log('callback');
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.write(result.rows[0].Name + "\n");
-            res.write(process.env.NODE_ENV + "\n");
+            //res.write(port);
+            res.write("process.env.NODE_ENV:" + process.env.NODE_ENV + "\n");
             res.end('Hello World\n');
             });
 
