017e8740a3e01b05445b9ce3bf4eb68a8a5640bb
conradj <conradsjackson@gmail.com>
2015-11-08 22:09:55 +0000

    remove comments

--- common/models/location.js
+++ common/models/location.js
@@ -1,31 +1,6 @@
 module.exports = function(Location) {
     Location.validatesUniquenessOf('name');
     
-//    Location.observe('access', function logQuery(ctx, next) {
-//      console.log('Accessing %s matching %s', ctx.Model.modelName, ctx.query.where);
-//      console.log('Accessing request:', ctx.req);
-//      console.log('Accessing result:', ctx.res);
-//      next();
-//    });
-//    
-//    Location.observe('before save', function (ctx, next) {
-//      console.log("before save?");
-//      
-//        console.log("bob:", ctx.instance);
-//      console.log('before request:', ctx.req);
-//        console.log('before result:', ctx.res);
-//      next();
-//    });
-//    
-//    Location.observe('after save', function (ctx, next) {
-//      console.log("after save?");
-//        console.log("after save:", ctx.instance);
-//        console.log('after request:', ctx.req);
-//        console.log('after result:', ctx.res);
-//        Location.app.io.emit('roommsg', ctx.instance);
-//      next();ss
-//    });
-    
     Location.findOrCreateOnName = function(name, cb) {
         Location.findOrCreate({where: {"Name": name }}, {"Name": name }, function(err, instance) {
             if (err){
