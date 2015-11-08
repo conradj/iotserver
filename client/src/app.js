49d00817ec4961ea7efdd39f2512c5083d361a2e
conradj <conradsjackson@gmail.com>
2015-11-08 22:09:42 +0000

    update with proposed navigation

--- client/src/app.js
+++ client/src/app.js
@@ -2,10 +2,10 @@ export class App {
   configureRouter(config, router) {
     config.title = 'TotterUp';
     config.map([
-      { route: ['', 'home'], name: 'home',      moduleId: 'home',      nav: true, title: 'Home' },
-      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
-      { route: 'locations',         name: 'locations',        moduleId: 'locations',        nav: true, title: 'Github locations' },
-      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' }
+      { route: ['', 'home'], name: 'home', moduleId: 'home', nav: true, title: 'Music', settings: { icon: 'fa-music' } },
+      { route: 'users', name: 'users', moduleId: 'users', nav: true, title: 'Reading', settings: { icon: 'fa-quote-right' } },
+      { route: 'locations', name: 'locations', moduleId: 'locations', nav: true, title: 'Github locations', settings: { icon: 'fa-heart' } },
+      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Insights', settings: { icon: 'fa-line-chart' } }
     ]);
 
     this.router = router;
