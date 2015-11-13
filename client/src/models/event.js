d56123902cfcba05853fbec3294f24a2a3e0a62e
conradj <conradsjackson@gmail.com>
2015-11-13 23:37:21 +0000

    toggle display track attributes

--- client/src/models/event.js
+++ client/src/models/event.js
@@ -4,5 +4,10 @@ export class Event {
 	constructor(data) {
 		Object.assign(this, data);
 		this.playedFromNow = moment(this.CreateDate).fromNow();
+		this.viewAttributes = false;
 	}
+	
+	toggleTrackAttributes() {
+    	this.viewAttributes = !this.viewAttributes;
+  	}
 }
