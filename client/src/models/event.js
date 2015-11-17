b17f8cf587e0edda7e5be6ade3eca01ab87148bb
conradj <conradsjackson@gmail.com>
2015-11-17 22:12:49 +0000

    refactor to use value convertor for date fromNow strings

--- client/src/models/event.js
+++ client/src/models/event.js
@@ -1,9 +1,6 @@
-import moment from 'moment';
-
 export class Event {
 	constructor(data) {
 		Object.assign(this, data);
-		this.playedFromNow = moment(this.CreateDate).fromNow();
 		this.viewAttributes = false;
 	}
 	
