b501d6dd4c5abe45b2a9b5a0fc514fed0ff9d464
conradj <conradsjackson@gmail.com>
2015-12-04 10:17:02 +0000

    try and fix npm and node for azure again

--- deploy.sh
+++ deploy.sh
@@ -87,9 +87,9 @@ selectNodeVersion () {
       NODE_EXE=node
     fi
     # ensure node version is set in azure environment variables
-    #echo select npm version b $NPM_JS_PATH
-    #original line NPM_CMD="\"$NODE_EXE\" \"$NPM_JS_PATH\""
-    NPM_CMD="\"$NODE_EXE\" \"$PROGRAMFILES\\npm\\3.3.9\\node_modules\\npm\\bin\\npm-cli.js\""
+    echo select npm version b $NPM_JS_PATH
+    original line NPM_CMD="\"$NODE_EXE\" \"$NPM_JS_PATH\""
+    #NPM_CMD="\"$NODE_EXE\" \"$PROGRAMFILES\\npm\\3.3.9\\node_modules\\npm\\bin\\npm-cli.js\""
   else
     NPM_CMD=npm
     NODE_EXE=node
@@ -121,9 +121,9 @@ if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   echo NPM update.
   eval $NPM_CMD update
   exitWithMessageOnError "npm failed"
-  echo jspm install.
-  eval "node_modules/.bin/jspm install aurelia-animator-css aurelia-binding aurelia-bootstrapper aurelia-dependency-injection aurelia-framework aurelia-fetch-client aurelia-router aurelia-event-aggregator aurelia-history-browser aurelia-loader-default aurelia-loader aurelia-metadata aurelia-route-recognizer aurelia-templating-binding aurelia-templating-resources aurelia-templating-router aurelia-templating aurelia-logging aurelia-task-queue aurelia-history aurelia-path aurelia-pal aurelia-pal-browser"
-  exitWithMessageOnError "jspm failed"
+  #echo jspm install.
+  #eval "node_modules/.bin/jspm install aurelia-animator-css aurelia-binding aurelia-bootstrapper aurelia-dependency-injection aurelia-framework aurelia-fetch-client aurelia-router aurelia-event-aggregator aurelia-history-browser aurelia-loader-default aurelia-loader aurelia-metadata aurelia-route-recognizer aurelia-templating-binding aurelia-templating-resources aurelia-templating-router aurelia-templating aurelia-logging aurelia-task-queue aurelia-history aurelia-path aurelia-pal aurelia-pal-browser"
+  #exitWithMessageOnError "jspm failed"
   cd - > /dev/null
 fi
 
