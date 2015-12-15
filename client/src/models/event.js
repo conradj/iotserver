32cbb86c19e2d457269ec2a592b5d8f34d6d3515
conradj <conradsjackson@gmail.com>
2015-12-15 15:31:48 +0000

    experiment with musical attributes and animation

--- client/src/models/event.js
+++ client/src/models/event.js
@@ -1,3 +1,4 @@
+import {computedFrom} from 'aurelia-framework';
 import {Album} from './album';
 export class Event {
 	constructor(data) {
@@ -5,6 +6,11 @@ export class Event {
 		this.viewAttributes = false;
 		this.CreateDate = this.CreateDate || Date.now(); 
 		this.track[0].album = new Album(this.track[0].album);
+		this.hue = 240 + (this.track[0].audio.valence ? this.track[0].audio.valence * 100 : 0); // blue is 240, so anything higher gets redder
+		this.saturation = (this.track[0].audio.valence ? this.track[0].audio.valence * 100 : 0) + "%"; // 
+		this.lightness = (this.track[0].audio.valence ? this.track[0].audio.valence * 100 : 0) + "%"; //;
+		this.alpha = (this.track[0].audio.loudness ? Math.pow(10, this.track[0].audio.loudness / 20) : 0);
+		this.bpm = (this.track[0].audio.tempo ? 120000 / this.track[0].audio.tempo : 1000) + "ms";
 	}
 	
 	toggleTrackAttributes() {
