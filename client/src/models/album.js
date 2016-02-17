3eb13521dab829d5e2a04737e8e69ab9034be3af
conradj <conradsjackson@gmail.com>
2016-02-17 21:31:07 +0000

    remove computed property

--- client/src/models/album.js
+++ client/src/models/album.js
@@ -3,11 +3,12 @@ import {computedFrom} from 'aurelia-framework';
 export class Album {
 	constructor(data) {
 		Object.assign(this, data);
+        this.thumbnail = this.coverUrl ? this.coverUrl : '';
 	}
 	
-	@computedFrom('coverUrl')
-	get thumbnail() {
-		//console.log('coverUrl computed', this.Title, this.coverUrl)
-		return this.coverUrl ? this.coverUrl : '';
-	}
+	// @computedFrom('coverUrl')
+	// get thumbnail() {
+	// 	//console.log('coverUrl computed', this.Title, this.coverUrl)
+	// 	return this.coverUrl ? this.coverUrl : '';
+	// }
 }
