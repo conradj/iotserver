ea95ab0c28a2dcf772d0530b4c5b19b7588f672d
conradj <conradsjackson@gmail.com>
2016-02-17 21:26:05 +0000

    check for echonest data

--- client/src/models/event.js
+++ client/src/models/event.js
@@ -4,14 +4,21 @@ export class Event {
 	constructor(data) {
 		Object.assign(this, data);
 		this.viewAttributes = false;
-		this.CreateDate = this.CreateDate || Date.now(); 
-		this.track[0].album = new Album(this.track[0].album);
-		this.hue = 240 + (this.track[0].audio.valence ? this.track[0].audio.valence * 100 : 0); // blue is 240, so anything higher gets redder
-		this.saturation = (this.track[0].audio.valence ? this.track[0].audio.valence * 100 : 0) + "%"; // 
-		this.lightness = (this.track[0].audio.valence ? this.track[0].audio.valence * 100 : 0) + "%"; //;
-		this.alpha = (this.track[0].audio.loudness ? Math.pow(10, this.track[0].audio.loudness / 20) : 0);
-		this.bpm = (this.track[0].audio.tempo ? 120000 / this.track[0].audio.tempo : 1000) + "ms";
-		this.opacity = (this.track[0].audio.danceability ? this.track[0].audio.danceability : 0.5);
+		this.CreateDate = this.CreateDate || Date.now();
+		try {
+			this.track[0].album = new Album(this.track[0].album);
+		
+			if(this.track[0].audio) {
+				this.hue = 240 + (this.track[0].audio.valence ? this.track[0].audio.valence * 100 : 0); // blue is 240, so anything higher gets redder
+				this.saturation = (this.track[0].audio.valence ? this.track[0].audio.valence * 100 : 0) + "%"; // 
+				this.lightness = (this.track[0].audio.valence ? this.track[0].audio.valence * 100 : 0) + "%"; //;
+				this.alpha = (this.track[0].audio.loudness ? Math.pow(10, this.track[0].audio.loudness / 20) : 0);
+				this.bpm = (this.track[0].audio.tempo ? 120000 / this.track[0].audio.tempo : 0) + "ms";
+				this.opacity = (this.track[0].audio.danceability ? this.track[0].audio.danceability : 0.5);
+			}
+		} catch(e) {
+			console.log('ERROR', 'event ctor', data, e)
+		}
 	}
 	
 	
