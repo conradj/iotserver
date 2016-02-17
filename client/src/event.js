ae73f2631ec9b7c86408ab85bb29cfdfb0c59d65
conradj <conradsjackson@gmail.com>
2016-02-17 21:35:08 +0000

    show consequetive album tracks in one event

--- client/src/event.js
+++ client/src/event.js
@@ -1,11 +1,21 @@
-import {customElement, bindable} from 'aurelia-framework';
+import {customElement, bindable, inject, BindingEngine} from 'aurelia-framework';
 
+@inject(BindingEngine)
 @customElement('event')
 export class Event {
   @bindable event = {};
     
-  constructor() {
-    
+  constructor(bindingEngine) {
+    this.bindingEngine = bindingEngine;
+  }
+  
+  bind() {
+    if(this.event.eventGroup) {
+      // subscribe
+      let subscription = this.bindingEngine.collectionObserver(this.event.track).subscribe();
+      // unsubscribe
+      subscription.dispose();
+    }
   }
   
   toggleTrackAttributes() {
